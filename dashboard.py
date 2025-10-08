7084e5527e18874b8399857e9bfa1dae704a3c84
JMarkoFlores <jeanmpachecof@gmail.com>
2025-10-07 23:08:33 -0500

    LISTOOO

--- dashboard.py
+++ dashboard.py
@@ -30,7 +30,7 @@ st.markdown(
 # === SIDEBAR ===
 st.sidebar.title("Menú")
 opcion = st.sidebar.radio(
-    "Ejercicio - Capítulo 11:",
+    "Libro OpenCV:",
     ("Capitulo 1","Capitulo 2","Capitulo 3", "Capitulo 4", "Capitulo 5","Capitulo 6","Capitulo 7","Capitulo 8", "Capitulo 9" ,"Capitulo 10", "Capitulo 11")
 )
 
